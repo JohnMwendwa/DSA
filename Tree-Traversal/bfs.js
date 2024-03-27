68d6f9423764890238483b4c662653d2a2d812bd
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2024-03-27 09:04:50 +0300

    Create BFS method

---
+++ Tree-Traversal/bfs.js
@@ -0,0 +1,90 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val) {
+    let newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (val === current.val) return undefined;
+      if (val < current.val) {
+        if (!current.left) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+
+  find(val) {
+    if (!this.root) return false;
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (val < current.val) {
+        current = current.left;
+      } else if (val > current.val) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return false;
+    return current;
+  }
+
+  BFS() {
+    // Create a queue and a variable to store the values of nodes visited
+    // Place the root node in the queue
+    // Loop as long as there is anything in the queue
+    // Dequeue a node from the queue and push the value of the node into the variable that stores the nodes
+    // If there is a left property on the node dequeued - add it to the queue
+    // If there is a right property on the node dequeued - add it to the queue
+    // Return the variable that stores the values
+
+    let node = this.root,
+      data = [],
+      queue = [];
+
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+
+    return data;
+  }
+}
+
+let tree = new BST();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+
+console.log(tree.BFS()); // [10, 6, 15, 3, 8, 20]
